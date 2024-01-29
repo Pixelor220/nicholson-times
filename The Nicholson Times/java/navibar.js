a4b3fb84f143bd7090a0eb57f50b45562531c74e
Pixelor <99868483+Pixelor220@users.noreply.github.com>
2024-01-30 08:29:31 +0900

    Delete The Nicholson Times directory

--- The Nicholson Times/java/navibar.js
+++
@@ -1,1 +0,0 @@
-document.write('<div class="align"><ul><li id="link"><a href="index.html">HOME</a></li><li id="link"><a href="events.html">EVENTS</a></li><li id="link"><a href="#">EXTRAS</a></li></ul></div>');
