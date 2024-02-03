8c9cf355f6b3e7aaa54c33fe223cd564596349d7
Pixelor <99868483+Pixelor220@users.noreply.github.com>
2024-02-03 11:13:28 +0900

    Update navibar.js

--- java/navibar.js
+++ java/navibar.js
@@ -1,1 +1,1 @@
-document.write('<div class="align"><ul><li id="link"><a href="index.html">HOME</a></li><li id="link"><a href="events.html">EVENTS</a></li><li id="link"><a href="#">EXTRAS</a></li></ul></div>');
+document.write('<div class="align"><ul><li id="link"><a href="index.html">HOME</a></li><li id="link"><a href="events.html">EVENTS</a></li><li id="link"><a href="sites.html">WEBSITES</a></li></ul></div>');
